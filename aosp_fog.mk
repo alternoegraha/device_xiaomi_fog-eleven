121d0581427e09024354dc01738f877d24f2b3d7
alternoegraha <noegrahachan@gmail.com>
2024-01-12 08:15:41 +0000

    fog: Don't build Live Wallpapers
    Signed-off-by: alternoegraha <noegrahachan@gmail.com>

--- aosp_fog.mk
+++ aosp_fog.mk
@@ -14,6 +14,9 @@ TARGET_BOOT_ANIMATION_RES := 720
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_SUPPORTS_QUICK_TAP := false
 
+# Do not ship with live wallpapers
+TARGET_INCLUDE_LIVE_WALLPAPERS := false
+
 # Inherit from fog device
 $(call inherit-product, device/xiaomi/fog/device.mk)
 
