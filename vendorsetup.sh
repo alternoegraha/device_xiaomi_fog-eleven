46ab48ec9d168b490ca3036816d977e3140ec563
alternoegraha <noegrahachan@gmail.com>
2025-05-28 06:23:39 +0000

    fog: treewide: update
    vendorsetup: update
    remove old prebuilt kernel
    
    Signed-off-by: alternoegraha <noegrahachan@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,6 @@
 # Inherit some dependencies
 git clone -b lineage-18.1 https://github.com/Notkerd69/vendor_xiaomi_fog-db vendor/xiaomi/fog
-git clone https://github.com/alternoegraha/kernel_xiaomi_fog_header kernel/xiaomi/fog
+git clone -b prebuilt https://github.com/alternoegraha/device_xiaomi_fog-kernel device/xiaomi/fog-kernel
 git clone -b lineage-18.1 https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi
 
 # fixup hardware/xiaomi
