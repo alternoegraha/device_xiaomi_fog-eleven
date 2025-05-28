5808cbf82239d071a47652ee6dfb29e95ce8ad34
alternoegraha <noegrahachan@gmail.com>
2025-05-28 23:46:32 +0000

    fog: vendorsetup: clone old kernel header
    Unfortunately I can't use new kernel header from device/xiaomi/fog-kernel, using that will results compilation fail
    
    Signed-off-by: alternoegraha <noegrahachan@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,7 @@
 # Inherit some dependencies
 git clone -b lineage-18.1 https://github.com/Notkerd69/vendor_xiaomi_fog-db vendor/xiaomi/fog
 git clone -b prebuilt https://github.com/alternoegraha/device_xiaomi_fog-kernel device/xiaomi/fog-kernel
+git clone --depth 1 https://github.com/dblenk-project/kernel_xiaomi_fog_header kernel/xiaomi/fog
 git clone -b lineage-18.1 https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi
 
 # fixup hardware/xiaomi
